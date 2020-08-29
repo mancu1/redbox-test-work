8afab9c0b3a24c683ab4d62a8017a79fa6bc7883
Mansur Zakirov <mansur.zakirov@boolqa.com>
2020-08-29 04:43:55 +0300

    + add page wrapper for max width

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
-    <router-view />
+    <ContentWrapper>
+      <router-view />
+    </ContentWrapper>
   </div>
 </template>
 
 <script>
+import ContentWrapper from "@/components/pageLayout/ContentWrapper";
 export default {
-  name: "app"
+  name: "app",
+  components: { ContentWrapper }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/pageLayout/ContentWrapper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentWrapper"
+};
+</script>
