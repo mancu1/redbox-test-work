5d3f09d5435ad8c7e0fa0c33df0a50e1d07f74b0
Mansur Zakirov <mansur.zakirov@boolqa.com>
2020-08-29 04:43:17 +0300

    + css imports

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@/style/resetCss.css";
 import "./plugins/element.js";
+import "@/style/blocks.css";
 import "@/style/colors.css";
 import "@/style/font.css";
+import "@/style/margins.css";
 
 Vue.config.productionTip = false;
 
